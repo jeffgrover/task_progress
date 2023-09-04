43a90cbbe00e6bff9c23cb3c9f77f62d7cd76aed
Jeff Grover <grover.jeff@gmail.com>
2023-09-04 15:13:56 -0600

    Demo worthy first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# task_progress
-An exploration of Server-sent Events (SSE) in FastAPI
+# Task Progress
+
+An exploration of [Server-sent Events (SSE)](https://dev.to/karanpratapsingh/system-design-long-polling-websockets-server-sent-events-sse-1hip), [MongoDB](https://pymongo.readthedocs.io/en/stable/) and [BackgroundTasks](https://fastapi.tiangolo.com/tutorial/background-tasks/) in [FastAPI](https://fastapi.tiangolo.com/)
+
+### Installation
+
+Python environment required (see [pyenv](https://github.com/pyenv/pyenv#readme) install) then `pip install -r requirements.txt` to get the (few) required libraries.
+
+### Running
+
+In a terminal window, at the command prompt launch the service with `./tasks.py` (or `python tasks.py` if that doesn't work).  As with most FastAPI apps, the endpoints served are available at http://localhost:8000/docs.  
+
+The webpage for demonstrating the server-sent events functionality is served from the URL  http://localhost:8000/ ( which is actually http://localhost:8000/static/tasks.html ).
+
+You just enter a name for the task and how many seconds you want it to run.  Then when you hit the "Create Task" button, a POST request is made to the server to start the task, and the task gives progress feedback every second to an EventSource in JavaScript that is also registered to the progress URL on the server.  When the task is complete, the progress connection is broken and the web page shows the final status.
+
+(Hit the browser refresh button to play again, this is merely a simple demonstration app.)
+
---
+++ tasks.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+from fastapi import FastAPI, BackgroundTasks, HTTPException
+from fastapi.responses import StreamingResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+from pymongo import MongoClient
+from datetime import datetime
+from typing import List, Optional
+from uvicorn import run
+from uuid import uuid4
+from asyncio import sleep
+
+app = FastAPI()
+
+# Mount the "static" folder as a static directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# MongoDB Configuration
+client = MongoClient("mongodb://localhost:27017/")
+db = client["taskdb"]
+collection = db["tasks"]
+
+
+class TaskModel:
+    def __init__(self, name: str, number: int, id: Optional[str] = None, start: Optional[datetime] = None,
+                 end: Optional[datetime] = None, progress: Optional[int] = None):
+        self.id = id
+        self.name = name
+        self.number = number
+        self.start = start
+        self.end = end
+        self.progress = progress
+
+
+async def count_to_number(task_id: str, number: int, bg_task: BackgroundTasks):
+    start_time = datetime.now()
+    for i in range(1, number + 1):
+        # Update progress in MongoDB
+        collection.update_one({"id": task_id}, {"$set": {"progress": i}})
+        # Send progress to the browser via SSE (Server Sent Events)
+        bg_task.add_task(send_sse_event, task_id, i)
+        await sleep(1)
+    end_time = datetime.now()
+    # Update the end timestamp in MongoDB
+    collection.update_one({"id": task_id}, {"$set": {"end": end_time}})
+
+
+def send_sse_event(task_id: str, progress: int):
+    # In a real application, you would use a library like aiohttp to send SSE events to the browser.
+    # For simplicity, we'll print the events here.
+    print(f"SSE Event for Task {task_id}: Progress {progress}", flush=True)
+
+
+@app.get("/")
+async def read_index():
+    return FileResponse('static/tasks.html')
+
+
+@app.get("/favicon.ico")
+async def read_favicon():
+    return FileResponse('static/task.png')
+
+
+@app.post("/tasks")
+async def create_task(task_data: dict, bg_task: BackgroundTasks):
+    name = task_data.get("name")
+    number = task_data.get("number")
+
+    # Generate a unique ID for the task
+    task_id = str(uuid4())
+
+    # Create a new task document in MongoDB
+    task = TaskModel(name=name, number=number, id=task_id, start=datetime.now())
+    collection.insert_one(task.__dict__)
+
+    # Start the background task to count
+    bg_task.add_task(count_to_number, task_id, number, bg_task)
+
+    return {"task_id": task_id}
+
+
+@app.get("/tasks")
+async def get_tasks():
+    tasks = list(collection.find({}, {"_id": 0, "id": 1, "name": 1}))
+    return tasks
+
+
+@app.get("/tasks/{task_id}")
+async def get_task(task_id: str):
+    task = collection.find_one({"id": task_id}, {"_id": 0})
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+
+@app.get("/tasks/{task_id}/progress")
+async def task_progress_sse(task_id: str):
+    async def sse_stream():
+        max_number_reached = False
+        while not max_number_reached:
+            task = collection.find_one({"id": task_id}, {"_id": 0})
+            if task is None or task.get("progress") is None:
+                yield "data: Task not found or progress not available\n\n"
+            else:
+                progress = task["progress"]
+                yield f"data: {progress}\n\n"
+
+                # Check if the maximum number has been reached
+                max_number = task["number"]
+                if progress >= max_number:
+                    max_number_reached = True
+
+            await sleep(1)
+
+        # Send a final SSE event to signal the end
+        yield "data: Task completed\n\n"
+
+    return StreamingResponse(sse_stream(), media_type="text/event-stream")
+
+
+if __name__ == "__main__":
+   run("tasks:app", host="127.0.0.1", port=8000, reload=True)
